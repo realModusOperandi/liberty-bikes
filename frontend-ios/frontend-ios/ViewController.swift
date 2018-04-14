2f4a02e66f26e1fa4a89c03afc7a71b68fddd922
Liam Westby <liamwestby@icloud.com>
2018-04-14 11:23:57 -0500

    Create blank single view application

---
+++ frontend-ios/frontend-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  frontend-ios
+//
+//  Created by Liam Westby on 4/14/18.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
